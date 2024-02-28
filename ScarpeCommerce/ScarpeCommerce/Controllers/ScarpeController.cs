57ac2bb0ed1eea3a9f4d368b1e20ed503fceaaca
HachimEnnaji <ashennaji@hotmail.it>
2024-02-28 17:00:21 +0100

    Aggiungere i file di progetto.

---
+++ ScarpeCommerce/ScarpeCommerce/Controllers/ScarpeController.cs
@@ -0,0 +1,82 @@
+﻿using ScarpeCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace ScarpeCommerce.Controllers
+{
+    public class ScarpeController : Controller
+    {
+        // GET: Scarpe
+        public ActionResult Index()
+        {
+            List<Scarpe> scarpe = new List<Scarpe>();
+            string scarpeDB = ConfigurationManager.ConnectionStrings["ScarpeDB"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(scarpeDB);
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Scarpe";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Scarpe s = new Scarpe();
+                    s.ID = Convert.ToInt32(reader["IDScarpe"]);
+                    s.Nome = reader["Nome"].ToString();
+                    s.Descrizione = reader["Descrizione"].ToString();
+                    s.Prezzo = Convert.ToDecimal(reader["Prezzo"]);
+                    s.ImmagineCopertina = reader["ImgCopertina"].ToString();
+                    s.ImmagineAggiuntiva1 = reader["ImmagineAggiuntiva1"].ToString();
+                    s.ImmagineAggiuntiva2 = reader["ImmagineAggiuntiva2"].ToString();
+                    scarpe.Add(s);
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                ViewBag.Message = "Errore: " + ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View(scarpe);
+        }
+        public ActionResult Dettagli(string id)
+        {
+            Scarpe s = new Scarpe();
+            string scarpeDB = ConfigurationManager.ConnectionStrings["ScarpeDB"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(scarpeDB);
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Scarpe WHERE Nome = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    s.ID = Convert.ToInt32(reader["IDScarpe"]);
+                    s.Nome = reader["Nome"].ToString();
+                    s.Descrizione = reader["Descrizione"].ToString();
+                    s.Prezzo = Convert.ToDecimal(reader["Prezzo"]);
+                    s.ImmagineCopertina = reader["ImgCopertina"].ToString();
+                    s.ImmagineAggiuntiva1 = reader["ImmagineAggiuntiva1"].ToString();
+                    s.ImmagineAggiuntiva2 = reader["ImmagineAggiuntiva2"].ToString();
+                }
+            }
+            catch (SqlException ex)
+            {
+                ViewBag.Message = "Errore: " + ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View(s);
+        }
+    }
+}
---
+++ ScarpeCommerce/ScarpeCommerce/Models/Scarpe.cs
@@ -0,0 +1,14 @@
+﻿namespace ScarpeCommerce.Models
+{
+    public class Scarpe
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Prezzo { get; set; }
+        public string ImmagineCopertina { get; set; }
+        public string ImmagineAggiuntiva1 { get; set; }
+        public string ImmagineAggiuntiva2 { get; set; }
+
+    }
+}
